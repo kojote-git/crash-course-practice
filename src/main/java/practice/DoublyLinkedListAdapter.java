9f98b0356f76c9f1ba33b24bb4d2186397ea4433
Pavlo, Budantsev <pavlo.budantsev@gmail.com>
2021-10-28 20:56:20 +0300

    initial commit

---
+++ src/main/java/practice/DoublyLinkedListAdapter.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class DoublyLinkedListAdapter<T> extends AbstractCollection<T> {
+    private final DoublyLinkedList<T> delegate;
+
+    public DoublyLinkedListAdapter(DoublyLinkedList<T> list) {
+        this.delegate = list;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return delegate.iterator();
+    }
+
+    @Override
+    public int size() {
+        return delegate.size();
+    }
+}
